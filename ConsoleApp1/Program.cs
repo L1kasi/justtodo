9a10878adbf99a0808c67ab48662c391d6349473
L1kasi <46110956+L1kasi@users.noreply.github.com>
2018-12-26 00:35:33 +0500

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,12 +11,15 @@ namespace ConsoleApp1
         static void Main()
         {
             int a, b, c, n;
+            Console.WriteLine("Введите первую цифру");
             a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите вторую цифру");
             b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третью цифру");
             c = int.Parse(Console.ReadLine());
             n = a + b + c;
             Helloworld(n);
-            Console.WriteLine(n);
+            Console.WriteLine("количество написанных фраз,  а также сумма трех чисел", n);
         }
         public static void Helloworld(int n)
         {
