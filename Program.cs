e07a59c26a91ef540862ae095cbaf6e2edf3a788
L1kasi <46110956+L1kasi@users.noreply.github.com>
2018-12-25 15:03:15 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    static class Program
+    {
+
+        static void Main()
+        {
+            int a, b, c, n;
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Helloworld(n));
+
+        }
+        public static string Helloworld(int n)
+        {
+            string s, l;
+            s = "Привет мир! ";
+            l = null;
+            for (int i=0; i<n; i++)
+             l = l + s;
+            return l;
+        }
+    }
+}
