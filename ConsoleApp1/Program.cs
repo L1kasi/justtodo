11bd47c0b1281955a47e8e6997f43961cab90dcb
L1kasi <46110956+L1kasi@users.noreply.github.com>
2018-12-26 00:06:33 +0500

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Program
+    {
+        static void Main()
+        {
+            int a, b, c, n;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            n = a + b + c;
+            Itsme.Helloworld(n);
+            Console.WriteLine(n);
+        }
+        sealed class Itsme
+        {
+            public static void Helloworld(int n)
+            {
+                string s;
+                s = "Привет мир! ";
+                for (int i = 0; i < n; i++)
+                    Console.WriteLine(s);
+            }
+        }
+    }
+}
